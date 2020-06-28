7e532e64265b99741a0be740a3349c976e49aca8
DieMaskeLP <DieMaskeLP@users.noreply.github.com>
2020-06-28 13:38:00 +0200

    Init

--- bot.js
+++ bot.js
@@ -1,14 +1,23 @@
 const dc = require('discord.js')
 const fs = require("fs")
 const bot = new dc.Client();
-
+let isBotRunning = false
 const config = JSON.parse(fs.readFileSync("./bot_config.json"))
 
 bot.on("ready", function (){
     if (config.name) bot.user.setUsername(config.name)
+    if (config.activity) bot.user.setPresence({ "game": { name: config.activity }, status: 'online' })
     console.log(`${bot.user.tag} logged in`)
 })
+
+
+
+
 bot.on("message", function (msg){
+
+    if (msg.author.bot) return
+
+
     let data = {
         "author": msg.author,
         "channel": msg.channel,
@@ -22,6 +31,36 @@ bot.on("message", function (msg){
         "startsWithPrefix": (msg.content.startsWith(config.prefix)),
         "command": (msg.content.startsWith(config.prefix) ? msg.content.split(" ")[0].replace(config.prefix, "") : "null")
     }
-    data.channel.send(`MessageReceived: By: ${data.author.tag} in channel: ${(data.channelName)} with content: ${data.content}`, {})
+
+
+    if (data.startsWithPrefix){
+        switch (data.command){
+            case "hallo":
+                data.channel.send("Moinsen " + data.author.toString())
+                break
+            case "avatar":
+                if (data.args.length===1){
+                    data.channel.send(data.message.mentions.users.array()[0].avatarURL())
+                } else data.channel.send(data.author.avatarURL())
+                break
+            case "git":
+                data.channel.send("https://github.com/DieMaskeLP/discord_bot")
+                break
+        }
+    }
+
+
 })
-bot.login(config.token)
+
+
+
+function switchBotState(){
+    if (isBotRunning){
+        bot.destroy()
+    } else {
+        bot.login(config.token)
+    }
+    isBotRunning = !isBotRunning
+    return isBotRunning
+}
+switchBotState()
